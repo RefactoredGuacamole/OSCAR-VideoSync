1bdb44531c0ae0a1ed08643139b48ebada7f39cc
Refactored Guacamole <149190054+RefactoredGuacamole@users.noreply.github.com>
2023-10-27 12:42:49 -0400

    Focus

--- oscar/videosync.cpp
+++ oscar/videosync.cpp
@@ -5,10 +5,14 @@
 #include <QProcess>
 #include <QLabel>
 
+namespace {
+const char *SOCKET_NAME = "/tmp/mpvsocket";
+}
+
 VideoSync::VideoSync(QWidget *parent)
     : QWidget{parent}
 {
-    m_button1 = new QPushButton("Open MPV");
+    m_button1 = new QPushButton(tr("Open MPV"));
     auto* label = new QLabel();
 
     auto* layout = new QVBoxLayout();
@@ -19,23 +23,19 @@ VideoSync::VideoSync(QWidget *parent)
     m_mpvProcess = new QProcess(this);
 
     connect(m_button1, &QPushButton::clicked, this, &VideoSync::onOpenMpvClick);
-    connect(m_mpvProcess, &QProcess::stateChanged, label, [this, label] {
+    connect(m_mpvProcess, &QProcess::stateChanged, m_button1, [this] {
         if (m_mpvProcess->state() == QProcess::ProcessState::NotRunning) {
-            label->setText("NotRunning");
-        } else if (m_mpvProcess->state() == QProcess::ProcessState::Starting) {
-            label->setText("Starting");
-        } else if (m_mpvProcess->state() == QProcess::ProcessState::Running) {
-            label->setText("Running");
+            m_button1->setText(tr("Open MPV"));
+        } else {
+            m_button1->setText(tr("Focus MPV"));
         }
     });
 }
 
 void VideoSync::onOpenMpvClick() {
     if (m_mpvProcess->state() == QProcess::ProcessState::NotRunning) {
-        m_mpvProcess->start("/opt/homebrew/bin/mpv", {"--input-ipc-server=/tmp/mpvsocket", "--force-window", "--idle"});
-        m_button1->setText("Close MPV");
+        m_mpvProcess->start("/opt/homebrew/bin/mpv", {QString("--input-ipc-server=%s").arg(SOCKET_NAME), "--force-window", "--idle"});
     } else {
-        m_mpvProcess->terminate();
-        m_button1->setText("Open MPV");
+        // TODO focus mpv
     }
 }
